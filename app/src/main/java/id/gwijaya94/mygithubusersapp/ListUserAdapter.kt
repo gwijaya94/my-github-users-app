4b6ee38b69157f9f62bff5175af6038f213ec0dd
Gilbert <gilbertwijaya94@gmail.com>
2021-11-24 11:55:45 +0700

    implement recyclerview touchable

--- app/src/main/java/id/gwijaya94/mygithubusersapp/ListUserAdapter.kt
+++ app/src/main/java/id/gwijaya94/mygithubusersapp/ListUserAdapter.kt
@@ -1,6 +1,5 @@
 package id.gwijaya94.mygithubusersapp
 
-import android.R
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -10,9 +9,20 @@ import id.gwijaya94.mygithubusersapp.model.User
 
 class ListUserAdapter(private val listUsers: List<User>) :
     RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClicked(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+
     class ListViewHolder(var binding: ItemUserDataRowBinding) :
         RecyclerView.ViewHolder(binding.root)
 
+    interface OnItemClickCallback {
+        fun onItemClicked(data: User)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val binding =
@@ -31,8 +41,9 @@ class ListUserAdapter(private val listUsers: List<User>) :
         holder.binding.apply {
             listItemFollower.text = userData.follower
             listItemName.text = userData.name
-            
+
         }
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(userData) }
 
     }
 
--- app/src/main/java/id/gwijaya94/mygithubusersapp/activity/MainActivity.kt
+++ app/src/main/java/id/gwijaya94/mygithubusersapp/activity/MainActivity.kt
@@ -1,11 +1,13 @@
 package id.gwijaya94.mygithubusersapp.activity
 
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.gwijaya94.mygithubusersapp.ListUserAdapter
 import id.gwijaya94.mygithubusersapp.databinding.ActivityMainBinding
+import id.gwijaya94.mygithubusersapp.getIcon
 import id.gwijaya94.mygithubusersapp.getJsonDataFromAsset
 import id.gwijaya94.mygithubusersapp.model.User
 
@@ -16,6 +18,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        supportActionBar?.apply {
+            title = "Github Users"
+            setHomeAsUpIndicator(getIcon(this@MainActivity, "cmd_github", color = Color.WHITE))
+            setDisplayHomeAsUpEnabled(true)
+        }
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -23,6 +30,15 @@ class MainActivity : AppCompatActivity() {
         listUserData = jsonListUser.users
 
         val listUsersAdapter = ListUserAdapter(listUserData)
+        listUsersAdapter.setOnItemClicked(object : ListUserAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: User) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Kamu memilih " + data.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
         binding.rvListUsers.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@MainActivity)
