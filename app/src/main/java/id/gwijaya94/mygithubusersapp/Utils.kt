2033fa98e34fcc7f061eda51b63f8e25b9199ff5
Gilbert <gilbertwijaya94@gmail.com>
2021-11-24 11:54:28 +0700

    adding styling and function for getIcon

--- app/src/main/java/id/gwijaya94/mygithubusersapp/Utils.kt
+++ app/src/main/java/id/gwijaya94/mygithubusersapp/Utils.kt
@@ -1,7 +1,13 @@
 package id.gwijaya94.mygithubusersapp
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import com.google.gson.Gson
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.typeface.library.community.material.CommunityMaterial
+import com.mikepenz.iconics.utils.colorInt
+import com.mikepenz.iconics.utils.sizeDp
 import id.gwijaya94.mygithubusersapp.model.JsonListUser
 
 fun getJsonDataFromAsset(context: Context, fileName: String): JsonListUser {
@@ -10,3 +16,12 @@ fun getJsonDataFromAsset(context: Context, fileName: String): JsonListUser {
 
     return gson.fromJson(jsonFileString, JsonListUser::class.java)
 }
+
+fun getIcon(
+    context: Context, iconName: String, color: Int = Color.WHITE, size: Int = 24
+): Drawable = IconicsDrawable(context).apply {
+    icon = CommunityMaterial.getIcon(iconName)
+    colorInt = color
+    sizeDp = size
+}
+
